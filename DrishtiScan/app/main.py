ba70b3ff5d0ab6a555dc2387762141b73940c8d3
unknown <somiljaintkg1234@gmail.com>
2026-06-04 10:57:08 +0530

    bug fixes

--- DrishtiScan/app/main.py
+++ DrishtiScan/app/main.py
@@ -839,6 +839,13 @@ def yield_health():
 
 if __name__ == "__main__":
     import uvicorn
+    import os
 
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=port
+    )
 
